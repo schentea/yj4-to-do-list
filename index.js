0f18d220517870fb9d65e42cb090c5128bbddc3c
schentea <schentea@gmail.com>
2023-11-08 13:08:04 +0900

    hi

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello JS")
+// 변수 const = 상수(바뀔 수 없는 수)
+// const whoAmI = "??" => 기본 형식(시작은 소문자 띄어쓰기 시 대문자)
+const name = "재시크"
+// name = "최현기"
+
+// 변수 let = 변할 수 있는 수
+let hobby = "보드"
+console.log(hobby)
+
+hobby = "잠"
+console.log(hobby)
+
+const a = 10
+const b = 20
+
+console.log(name)
+console.log(name + hobby)
+console.log(a + b)
+
+// typeof = 변수 형식 확인
+// "", '' 모두 동일
+console.log(typeof hobby)
+console.log(typeof a)
+console.log("나의 이름은 " + name + "이고, " + "취미는 " + hobby + "입니다.")
+
+// `` = 백틱 사용 형식
+console.log(`나의 이름은 ${name}이고, 취미는 ${hobby}입니다.`)
+
+// 배열
+const arr =  [1, 2, 3]
+console.log(arr)
+console.log(arr[0]) // 배열에 저장된 값 중에 1만 출력 ( [] 사용 )
