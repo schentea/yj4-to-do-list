e5de50515539c4c2053a7fee1c1637a51baa895d
LEESEUNGMINI <dltmdals7295@naver.com>
2023-12-27 12:35:02 +0900

    asd

--- order.js
+++ order.js
@@ -1,51 +1,49 @@
 const lists = document.querySelectorAll("li:not(.dragging)");
 
 lists.forEach((list) => {
+  list.addEventListener("dragstart", () => {
+    list.classList = "dragging";
+  });
 
-    list.addEventListener("dragstart", () => {
-        list.classList = "dragging";
-    })
-
-    list.addEventListener("dragend", () => {
-        list.classList.remove("dragging");
-    });
-
+  list.addEventListener("dragend", () => {
+    list.classList.remove("dragging");
+  });
 });
 
-const initSortableList = e => {
-    e.preventDefault();
-    
-    const draggingItem = ul.querySelector(".dragging");
-    //...가져온 아이들을 하나하나 배열에 추가
-    const siblings = [...ul.querySelectorAll("li:not(.dragging)")];
+const initSortableList = (e) => {
+  e.preventDefault();
 
-    let nextSibling = siblings.find((sibling) => {
-        return e.clientY <=sibling.offsetTop + sibling.offsetHeight / 2;
-    });
+  const draggingItem = ul.querySelector(".dragging");
+  //...가져온 아이들을 하나하나 배열에 추가
+  const siblings = [...ul.querySelectorAll("li:not(.dragging)")];
 
-    ul.insertBefore(draggingItem, nextSibling);
+  let nextSibling = siblings.find((sibling) => {
+    return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  });
+
+  ul.insertBefore(draggingItem, nextSibling);
 };
 
 function saveTo() {
-    localStorage.setItem("Todos", JSON.stringify(todos));
+  localStorage.setItem("Todos", JSON.stringify(todos));
 }
 
 function reSave(newLi) {
-    todos = [];
-    newLi.forEach(item => {
-        const text = item.querySelector("span")
-        const newTodoObject = {
-            text : text.innerText,
-            id : item.id
-        }
-        todos.push(newTodoObject);
-        saveTo();
-    })
+  todos = [];
+  newLi.forEach((item) => {
+    const text = item.querySelector("span");
+    const newTodoObject = {
+      text: text.innerText,
+      id: item.id,
+    };
+    todos.push(newTodoObject);
+    saveTo();
+  });
 }
 
 ul.addEventListener("dragover", initSortableList);
-ul.addEventListener("dragenter",(e) => e.preventDefault());
+ul.addEventListener("dragenter", (e) => e.preventDefault());
 ul.addEventListener("drop", () => {
-    const newLi = document.querySelectorAll("li");
-    reSave(newLi);
-})
+  const newLi = document.querySelectorAll("li");
+  reSave(newLi);
+});
--- randImage.js
+++ randImage.js
@@ -1,7 +1,7 @@
 const image = document.querySelector("#randImage");
-const images = ["test1.png","test2.png","test3.png"];
+const images = ["test1.png", "test2.png", "test3.png"];
 
-const selectImage = images[Math.floor(Math.random()*images.length)];
+const selectImage = images[Math.floor(Math.random() * images.length)];
 
 const img = document.createElement("img");
 img.setAttribute("src", `image/${selectImage}`);
